94848d6f3226fc749088ed59909bac8513cab2f0
cc <zhangguochang@bcwvip.com>
2017-08-16 20:42:25 +0800

    add prototype

---
+++ Prototype/Demo.js
@@ -0,0 +1,35 @@
+/**
+ * Created by cc on 17/8/16.
+ */
+
+/* class extends*/
+
+
+function Parent(name,age){
+    this.name = name
+    this.age = age
+    this.say = function(name,age){
+        console.log(name+' 1 '+age)
+    }
+}
+
+Parent.prototype = {
+    speakSometing(){
+        console.log("I can speek chinese");
+    }
+}
+
+function Child(name,age){
+    this.name = name
+    this.age = age
+}
+
+Child.prototype = new Parent()
+
+// Child.prototype = {
+//
+// }
+
+var c = new Child('job',30)
+
+console.log(c)
